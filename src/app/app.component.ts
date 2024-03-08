6dd5b2deee780c1b77da951c4b866a47a60b9a0f
ArmitaKoleini <armitakoleini02@gmail.com>
2024-03-08 20:50:04 +0330

    create new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WordCloudComponent } from './word-cloud/word-cloud.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,WordCloudComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/word-cloud/word-cloud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-cloud',
+  standalone: true,
+  imports: [],
+  templateUrl: './word-cloud.component.html',
+  styleUrl: './word-cloud.component.css',
+})
+export class WordCloudComponent {}
