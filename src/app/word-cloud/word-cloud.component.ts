4a9dd5cbecfd986f78ac67c781d63084b71b4dc4
ArmitaKoleini <armitakoleini02@gmail.com>
2024-03-09 17:35:34 +0330

    add color for input value

--- src/app/word-cloud/word-cloud.component.ts
+++ src/app/word-cloud/word-cloud.component.ts
@@ -13,10 +13,38 @@ export class WordCloudComponent {
   @Input() words: IWords[] = [];
   @Input() text: string = '';
 
+  getRandomColor(): string {
+    const colors = [
+      '#f44336',
+      '#e91e63',
+      '#9c27b0',
+      '#673ab7',
+      '#3f51b5',
+      '#2196f3',
+      '#03a9f4',
+      '#00bcd4',
+      '#009688',
+      '#4caf50',
+      '#8bc34a',
+      '#cddc39',
+      '#ffeb3b',
+      '#ffc107',
+      '#ff9800',
+      '#ff5722',
+      '#795548',
+      '#607d8b',
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
   addWord() {
-    const divElement = document.getElementById('random-word');
-    if (divElement) {
+    const element = document.getElementById('random-word');
+    if (element) {
+      const divElement = document.createElement('span');
       divElement.innerHTML += this.text;
+      const randomColor = this.getRandomColor()
+      divElement.style.color = randomColor ;
+      element.appendChild(divElement);
     }
   }
 
