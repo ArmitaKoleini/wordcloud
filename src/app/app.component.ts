61e098cf4ddf49c2000abcce143be0dba8b17ab9
ArmitaKoleini <armitakoleini02@gmail.com>
2024-03-09 16:00:04 +0330

    create generate button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WordCloudComponent } from './word-cloud/word-cloud.component';
+import { IWords } from './word-interface';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,WordCloudComponent],
+  imports: [RouterOutlet, WordCloudComponent, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'wordcloud';
+  @ViewChild('wordcloud') wordCloud!: WordCloudComponent;
+
+  words: IWords[] = [
+    { text: 'HTTP', score: 16, color: 'gold' },
+    { text: 'Ember', score: 8, color: 'aquamarine' },
+    { text: 'Sass', score: 20, color: 'olive' },
+    { text: 'HTML', score: 32, color: 'lightpink' },
+    { text: 'FlexBox', score: 24, color: 'mediumpurple' },
+    { text: 'API', score: 16, color: 'aquamarine' },
+    { text: 'VueJS', score: 20, color: 'lightblue' },
+    { text: 'Grid', score: 24, color: 'gold' },
+    { text: 'Rest', score: 12, color: 'crimson' },
+    { text: 'JavaScript', score: 36, color: 'hotpink' },
+    { text: 'Animation', score: 12, color: 'palegreen' },
+    { text: 'React', score: 28, color: 'pink' },
+    { text: 'CSS', score: 32, color: 'indigo' },
+    { text: 'Cache', score: 24, color: 'palegreen' },
+    { text: 'Less', score: 12, color: 'mediumpurple' },
+  ];
+
+  showRandomWord() {
+    this.wordCloud.generateRandomWord();
+  }
 }
--- src/app/word-cloud/word-cloud.component.ts
+++ src/app/word-cloud/word-cloud.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IWords } from '../word-interface';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-word-cloud',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './word-cloud.component.html',
   styleUrl: './word-cloud.component.css',
 })
-export class WordCloudComponent {}
+export class WordCloudComponent {
+  @Input() words: IWords[] = [];
+
+  generateRandomWord() {
+    const randomIndex = Math.floor(Math.random() * this.words.length);
+    const randomWord = this.words[randomIndex];
+
+    const element = document.getElementById('random-word');
+    if (element) {
+      const spanElement = document.createElement('span');
+      spanElement.textContent = randomWord.text;
+      spanElement.style.fontSize = `${randomWord.score}px`;
+      spanElement.style.color = randomWord.color;
+      element.appendChild(spanElement); // Add new word as child
+    }
+  }
+}
