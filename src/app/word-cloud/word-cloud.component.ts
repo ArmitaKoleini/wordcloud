71165e43de0f0e1e1ac3dbc44abe6527f04542b7
ArmitaKoleini <armitakoleini02@gmail.com>
2024-03-10 17:42:07 +0330

    create edit modal

--- src/app/word-cloud/word-cloud.component.ts
+++ src/app/word-cloud/word-cloud.component.ts
@@ -1,17 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Renderer2 } from '@angular/core';
 import { IWords } from '../word-interface';
 import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-word-cloud',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './word-cloud.component.html',
   styleUrl: './word-cloud.component.css',
 })
 export class WordCloudComponent {
   @Input() words: IWords[] = [];
   @Input() text: string = '';
+  editedUser: string = '';
+  indexToEdit: number = 0;
+
+  constructor(private renderer: Renderer2) {}
 
   getRandomColor(): string {
     const colors = [
@@ -34,17 +39,44 @@ export class WordCloudComponent {
   }
 
   createRandomizedSpanElement(text: string) {
-    const spanElement = document.createElement('span');
-    spanElement.textContent = text;
-    spanElement.style.cssText = `font-size: ${this.getRandomSize()}px; color: ${this.getRandomColor()}; float: left;`;
+    const spanElement = this.renderer.createElement('span');
+    this.renderer.addClass(spanElement, 'word');
+    this.renderer.setProperty(spanElement, 'textContent', text);
+    this.renderer.setStyle(
+      spanElement,
+      'font-size',
+      `${this.getRandomSize()}px`
+    );
+    this.renderer.setStyle(spanElement, 'color', this.getRandomColor());
+    this.renderer.setStyle(spanElement, 'float', 'left');
+    this.renderer.setProperty(spanElement, 'click', this.openModal.bind(this));
     return spanElement;
   }
 
   addWord() {
     const element = document.getElementById('random-word');
     if (element) {
-      element.appendChild(this.createRandomizedSpanElement(this.text));
+      const spanElement = this.createRandomizedSpanElement(this.text);
+      element.appendChild(spanElement);
     }
   }
 
+  public openModal(index: number) {
+    const modalElement = document.querySelector('.modal');
+    this.renderer.setStyle(modalElement, 'display', 'block');
+    const backDropElement = document.querySelector('.backdrop');
+    this.renderer.setStyle(backDropElement, 'display', 'block');
+    this.indexToEdit = index;
+  }
+  public closeModal() {
+    const modalElement = document.querySelector('.modal');
+    this.renderer.setStyle(modalElement, 'display', 'none');
+    const backDropElement = document.querySelector('.backdrop');
+    this.renderer.setStyle(backDropElement, 'display', 'none');
+  }
+
+  public onEdit() {
+    this.words[this.indexToEdit].text = this.editedUser;
+    this.editedUser = '';
+  }
 }
